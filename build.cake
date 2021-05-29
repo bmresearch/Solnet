2478904ec1af340158f4b64698d0b1f76db4ad2f
hugo <murlux@protonmail.com>
2021-05-29 13:24:46 +0100

    Adding missing code and cake build (#6)
    * Adding missing code and cake build
    
    * Fixing tests and adding more configs and dependencies to generate coverage reports.

---
+++ build.cake
@@ -0,0 +1,86 @@
+#module nuget:?package=Cake.DotNetTool.Module&version=0.4.0
+#addin nuget:?package=Cake.Coverlet&version=2.5.4
+#tool dotnet:?package=dotnet-reportgenerator-globaltool&version=4.8.7
+
+var target = Argument("target", "Publish");
+var configuration = Argument("configuration", "Release");
+var solutionFolder = "./";
+var outputFolder = "./artifacts";
+var reportTypes = "Html";
+var coverageFolder = "./code_coverage";
+
+
+var coberturaFileName = "results";
+var coberturaFileExtension = ".cobertura.xml";
+var coverageFilePath = Directory(coverageFolder) + File(coberturaFileName + coberturaFileExtension);
+
+Task("Clean")
+    .Does(() => {
+        CleanDirectory(outputFolder);
+        CleanDirectory(coverageFolder);
+    });
+
+Task("Restore")
+    .Does(() => {
+    DotNetCoreRestore(solutionFolder);
+    });
+
+Task("Build")
+    .IsDependentOn("Clean")
+    .IsDependentOn("Restore")
+    .Does(() => {
+        DotNetCoreBuild(solutionFolder, new DotNetCoreBuildSettings
+        {
+            NoRestore = true,
+            Configuration = configuration
+        });
+    });
+
+    
+Task("Test")
+    .IsDependentOn("Build")
+    .Does(() => {
+    
+        var coverletSettings = new CoverletSettings {
+            CollectCoverage = true,
+            CoverletOutputFormat = CoverletOutputFormat.cobertura | CoverletOutputFormat.opencover,
+            CoverletOutputDirectory = coverageFolder,
+            CoverletOutputName = coberturaFileName
+        };
+
+        var testSettings = new DotNetCoreTestSettings
+        {
+            NoRestore = true,
+            Configuration = configuration,
+            NoBuild = true
+        };
+
+        DotNetCoreTest("./test/Solnet.Rpc.Test/Solnet.Rpc.Test.csproj", testSettings, coverletSettings);
+    });
+
+
+Task("Report")
+    .IsDependentOn("Test")
+    .Does(() =>
+{
+    var reportSettings = new ReportGeneratorSettings
+    {
+        ArgumentCustomization = args => args.Append($"-reportTypes:{reportTypes}")
+    };
+    ReportGenerator(coverageFilePath, Directory(coverageFolder), reportSettings);
+});
+
+
+Task("Publish")
+    .IsDependentOn("Report")
+    .Does(() => {
+        DotNetCorePublish(solutionFolder, new DotNetCorePublishSettings
+        {
+            NoRestore = true,
+            Configuration = configuration,
+            NoBuild = true,
+            OutputDirectory = outputFolder
+        });
+    });
+
+RunTarget(target);
---
+++ src/Solnet.Util/AddressExtensions.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Solnet.Util
+{
+    public static class AddressExtensions
+    {
+        /// <summary>
+        /// Derives a program address.
+        /// </summary>
+        /// <param name="seeds">The address seeds.</param>
+        /// <param name="programId">The program Id.</param>
+        /// <returns>The address derived.</returns>
+        /// <exception cref="ArgumentException">Throws exception when one of the seeds has an invalid length.</exception>
+        /// <exception cref="Exception">Throws exception when the resulting address doesn't fall off the Ed25519 curve.</exception>
+        public static byte[] CreateProgramAddress(IList<byte[]> seeds, byte[] programId)
+        {
+            var buffer = new List<byte>();
+
+            foreach (var seed in seeds)
+            {
+                if (seed.Length > 32)
+                {
+                    throw new ArgumentException("max seed length exceeded", nameof(seeds));
+                }
+                buffer.AddRange(seed);
+            }
+            
+            buffer.AddRange(programId);
+            buffer.AddRange(Encoding.ASCII.GetBytes("ProgramDerivedAddress"));
+
+            var hash = SHA256.HashData(buffer.ToArray());
+
+            if (hash.IsOnCurve() != 0)
+            {
+                throw new Exception("invalid seeds, address must fall off curve");
+            }
+
+            return hash;
+        }
+        
+        /// <summary>
+        /// Attempts to find a program address for the passed seeds and program Id.
+        /// </summary>
+        /// <param name="seeds">The address seeds.</param>
+        /// <param name="programId">The program Id.</param>
+        /// <returns>A tuple corresponding to the address and nonce found.</returns>
+        /// <exception cref="Exception">Throws exception when it is unable to find a viable nonce for the address.</exception>
+        public static (byte[] Address, int Nonce) FindProgramAddress(IEnumerable<byte[]> seeds, byte[] programId)
+        {
+            var nonce = 255;
+            var buffer = seeds.ToList();
+
+            while (nonce-- != 0)
+            {
+                byte[] address;
+                try
+                {
+                    buffer.Add(new [] { (byte) nonce });
+                    address = CreateProgramAddress(buffer, programId);
+                }
+                catch (Exception)
+                {
+                    buffer.RemoveAt(buffer.Count - 1);
+                    continue;
+                }
+                
+                return (address, nonce);
+            }
+
+            throw new Exception("unable to find viable program address nonce");
+        }
+    }
+}
