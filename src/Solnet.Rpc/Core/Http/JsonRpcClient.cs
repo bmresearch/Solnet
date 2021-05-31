22f0a5d2123e213148d0de52c74e5ad8efc2c3be
Tiago Carvalho <tiagocarvalho92@hotmail.com>
2021-05-31 21:05:23 +0100

    Increasing robusteness of the http client (#22)
    * Proper error handling during HTTP JsonRpc requests

--- src/Solnet.Rpc/Core/Http/JsonRpcClient.cs
+++ src/Solnet.Rpc/Core/Http/JsonRpcClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -29,19 +30,61 @@ namespace Solnet.Rpc.Core.Http
 
         protected async Task<RequestResult<T>> SendRequest<T>(JsonRpcRequest req)
         {
-            JsonContent content = JsonContent.Create(req,  null, _serializerOptions);
-            Console.WriteLine($"\tRequest: {content.ReadAsStringAsync().Result}");
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/", req, _serializerOptions);
+            var requestJson = JsonSerializer.Serialize(req, _serializerOptions);
 
-            var tmp = await response.Content.ReadAsStringAsync();
+            //#TODO: replace with proper logging
+            Console.WriteLine($"\tRequest: {requestJson}");
+            HttpResponseMessage response = null;
+            RequestResult<T> result = null;
 
-            Console.WriteLine($"\tResult: {tmp}");
-
-            RequestResult<T> result = new RequestResult<T>(response);
-            if (result.WasSuccessful)
+            try
             {
-                var res = await response.Content.ReadFromJsonAsync<JsonRpcResponse<T>>(_serializerOptions);
-                result.Result = res.Result;
+                response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "/") { Content = new StringContent(requestJson, Encoding.UTF8, "application/json") });
+            }
+            catch (HttpRequestException e)
+            {
+                result = new RequestResult<T>(e.StatusCode ?? System.Net.HttpStatusCode.BadRequest, e.Message);
+            }
+            catch (Exception e)
+            {
+                result = new RequestResult<T>(System.Net.HttpStatusCode.BadRequest, e.Message);
+            }
+
+            result = new RequestResult<T>(response);
+            if (result.WasHttpRequestSuccessful)
+            {
+                try
+                {
+                    var requestRes = await response.Content.ReadAsStringAsync();
+
+                    //#TODO: replace with proper logging
+                    Console.WriteLine($"\tResult: {requestRes}");
+                    var res = JsonSerializer.Deserialize<JsonRpcResponse<T>>(requestRes, _serializerOptions);
+
+
+                    if (res.Result != null)
+                    {
+                        result.Result = res.Result;
+                    }
+                    else
+                    {
+                        var errorRes = JsonSerializer.Deserialize<JsonRpcErrorResponse>(requestRes, _serializerOptions);
+                        if (errorRes != null && errorRes.Error != null)
+                        {
+                            result.Reason = errorRes.Error.Message;
+                            result.ServerErrorCode = errorRes.Error.Code;
+                        }
+                        else
+                        {
+                            result.Reason = "Something wrong happened.";
+                        }
+                    }
+                }
+                catch (JsonException e)
+                {
+                    result.WasRequestSuccessfullyHandled = false;
+                    result.Reason = "Unable to parse json.";
+                }
             }
             return result;
         }
