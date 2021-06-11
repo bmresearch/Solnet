285bf4aa8fb4c826a3e78a449bccc1d362000f55
hugo <murlux@protonmail.com>
2021-06-11 20:43:06 +0100

    Improving keystore to allow restoring wallet from string byte array. (#87)
    * Improve keystore to allow restoring wallets from string byte array

--- src/Solnet.KeyStore/SolanaKeyStore.cs
+++ src/Solnet.KeyStore/SolanaKeyStore.cs
@@ -9,30 +9,25 @@ namespace Solnet.KeyStore
     /// </summary>
     public class SolanaKeyStoreService
     {
+        /// <summary>
+        /// Restores a keypair from a keystore compatible with the solana-keygen made in rust.
+        /// </summary>
+        /// <param name="privateKey">The string with the private key bytes.</param>
+        /// <param name="passphrase">The passphrase used while originally generating the keys.</param>
+        public Wallet.Wallet RestoreKeystore(string privateKey, string passphrase = "")
+        {
+            return InitializeWallet(privateKey.FromStringByteArray(), passphrase);
+        }
 
         /// <summary>
         /// Restores a keypair from a keystore compatible with the solana-keygen made in rust.
         /// </summary>
         /// <param name="path">The path to the keystore.</param>
         /// <param name="passphrase">The passphrase used while originally generating the keys.</param>
-        public Wallet.Wallet RestoreKeystore(string path, string passphrase = "")
+        public Wallet.Wallet RestoreKeystoreFromFile(string path, string passphrase = "")
         {
             var inputBytes = File.ReadAllText(path).FromStringByteArray();
-
-            var wallet = new Wallet.Wallet(inputBytes, passphrase, SeedMode.Bip39);
-
-            return wallet;
-        }
-
-        /// <summary>
-        /// NOT IMPLEMENTED.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        /// <exception cref="System.NotImplementedException">NOT IMPLEMENTED.</exception>
-        public Wallet.Wallet DecryptAndRestoreKeystore(string path)
-        {
-            throw new System.NotImplementedException();
+            return InitializeWallet(inputBytes, passphrase);
         }
 
         /// <summary>
@@ -46,14 +41,14 @@ namespace Solnet.KeyStore
         }
 
         /// <summary>
-        /// NOT IMPLEMENTED.
+        /// Initialize the wallet.
         /// </summary>
-        /// <param name="path"></param>
-        /// <param name="wallet">The wallet to save to the keystore.</param>
-        /// <exception cref="System.NotImplementedException">NOT IMPLEMENTED.</exception>
-        public void EncryptAndSaveKeystore(string path, Wallet.Wallet wallet)
+        /// <param name="seed">The seed.</param>
+        /// <param name="passphrase">The passphrase.</param>
+        /// <returns>The wallet.</returns>
+        private Wallet.Wallet InitializeWallet(byte[] seed, string passphrase = "")
         {
-            throw new System.NotImplementedException();
+            return new Wallet.Wallet(seed, passphrase, SeedMode.Bip39);
         }
     }
 }
