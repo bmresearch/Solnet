2c86dd87bea5b84ac4afde7d5c905b059866fe23
murlux <murlux@protonmail.com>
2021-07-02 23:14:06 +0100

    Undoing previous refactoring

--- src/Solnet.Rpc/Utilities/AddressExtensions.cs
+++ src/Solnet.Rpc/Utilities/AddressExtensions.cs
@@ -59,21 +59,22 @@ namespace Solnet.Rpc.Utilities
         /// <param name="programId">The program Id.</param>
         /// <returns>A tuple corresponding to the address and nonce found.</returns>
         /// <exception cref="Exception">Throws exception when it is unable to find a viable nonce for the address.</exception>
-        public static (byte[] Address, int Nonce) FindProgramAddress(IList<byte[]> seeds, byte[] programId)
+        public static (byte[] Address, int Nonce) FindProgramAddress(IEnumerable<byte[]> seeds, byte[] programId)
         {
             int nonce = 255;
+            List<byte[]> buffer = seeds.ToList();
 
             while (nonce-- != 0)
             {
                 byte[] address;
                 try
                 {
-                    seeds.Add(new[] { (byte)nonce });
-                    address = CreateProgramAddress(seeds, programId);
+                    buffer.Add(new[] { (byte)nonce });
+                    address = CreateProgramAddress(buffer, programId);
                 }
                 catch (Exception)
                 {
-                    seeds.RemoveAt(seeds.Count - 1);
+                    buffer.RemoveAt(buffer.Count - 1);
                     continue;
                 }
 
