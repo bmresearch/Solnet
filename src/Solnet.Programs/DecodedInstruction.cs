4e62242b710be5aad4d655fc4875d04d496378c2
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2021-07-27 11:26:01 -0700

    Implement InstructionDecoder (#161)

---
+++ src/Solnet.Programs/DecodedInstruction.cs
@@ -0,0 +1,15 @@
+﻿// unset
+
+using Solnet.Wallet;
+using System.Collections.Generic;
+
+namespace Solnet.Programs
+{
+    public class DecodedInstruction
+    {
+        public PublicKey PublicKey { get; set; }
+        public string ProgramName { get; set; }
+        public string InstructionName { get; set; }
+        public Dictionary<string, object> Values { get; set; }
+    }
+}
