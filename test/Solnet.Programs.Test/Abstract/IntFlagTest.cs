00d30bf646883398d0cac9784b0c370385c5900b
hugo <murlux@protonmail.com>
2021-08-08 22:15:54 +0100

    Added new flags and reworked old tests to use reflection (#178)
    * Added new flags and reworked old tests to use reflection

--- test/Solnet.Programs.Test/Abstract/IntFlagTest.cs
+++ test/Solnet.Programs.Test/Abstract/IntFlagTest.cs
@@ -6,12 +6,12 @@ using System.Reflection;
 namespace Solnet.Programs.Test.Abstract
 {
     [TestClass]
-    public class ByteFlagTest
-    {        
+    public class IntFlagTest
+    {
         [TestMethod]
         public void TestAllBitsSet()
         {
-            ByteFlag sut = new (byte.MaxValue);
+            IntFlag sut = new (uint.MaxValue);
 
             PropertyInfo[] props = sut.GetType().GetProperties();
 
@@ -35,7 +35,7 @@ namespace Solnet.Programs.Test.Abstract
         [TestMethod]
         public void TestNoBitsSet()
         {
-            ByteFlag sut = new (byte.MinValue);
+            IntFlag sut = new (uint.MinValue);
 
             PropertyInfo[] props = sut.GetType().GetProperties();
 
@@ -59,7 +59,7 @@ namespace Solnet.Programs.Test.Abstract
         [TestMethod]
         public void TestIndividualBitSet()
         {
-            PropertyInfo[] props = typeof(ByteFlag).GetProperties();
+            PropertyInfo[] props = typeof(IntFlag).GetProperties();
 
             foreach (PropertyInfo prop in props)
             {
@@ -75,7 +75,7 @@ namespace Solnet.Programs.Test.Abstract
                 byte bitNumber = byte.Parse(methodName.Split("_Bit")[1].Split("()")[0]);
                 double bitMaskValue = Math.Pow(2, bitNumber);
                 
-                ByteFlag sut = new ((byte) bitMaskValue);
+                IntFlag sut = new ((uint) bitMaskValue);
 
                 object isBitSet = getMethod.Invoke(sut, null);
                 Assert.IsNotNull(isBitSet);
