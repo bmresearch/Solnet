d43946d5cd0a9a78fb0a7859890a44419cc0735e
liquizard <mliquizard@gmail.com>
2021-08-13 21:06:23 +0100

    Mock testing for TokenWallet

---
+++ test/Solnet.Extensions.Test/MockTokenWalletRpc.cs
@@ -0,0 +1,115 @@
+﻿using Solnet.Rpc.Core.Http;
+using Solnet.Rpc.Messages;
+using Solnet.Rpc.Models;
+using Solnet.Rpc.Types;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Solnet.Extensions.Test
+{
+    internal class MockTokenWalletRpc : ITokenWalletRpcProxy
+    {
+
+        /// <summary>
+        /// Queue of RPC responses
+        /// </summary>
+        private Queue<string> _responses;
+
+        /// <summary>
+        /// The request sequence number
+        /// </summary>
+        private int _reqSeqId;
+
+        /// <summary>
+        /// The Json serializer options to be reused between calls.
+        /// </summary>
+        private readonly JsonSerializerOptions _serializerOptions;
+
+        /// <summary>
+        /// Constructor for the MockWalletRpc
+        /// </summary>
+        public MockTokenWalletRpc()
+        {
+            _reqSeqId = 0;
+            _responses = new Queue<string>();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters =
+                {
+                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+                }
+            };
+        }
+
+
+        public void Add(string resp)
+        {
+            _responses.Enqueue(resp);
+        }
+
+        
+        private async Task<RequestResult<ResponseValue<T>>> MockResult<T>()
+        {
+            // make sure we've got a queued response 
+            if (_responses.Count == 0) throw new ApplicationException("Mock responses exhausted");
+
+            // thread-safe increment response id + dequeue
+            int id = 0;
+            string json = null;
+            lock (this)
+            {
+                _reqSeqId++;
+                id = _reqSeqId;
+                json = _responses.Dequeue();
+            }
+
+            // deserialize JSON RPC response
+            var result = JsonSerializer.Deserialize<JsonRpcResponse<ResponseValue<T>>>(json, _serializerOptions);
+            if (result == null) throw new ApplicationException("Mock response did not deserialize");
+
+            // overwrite response id
+            result.Id = id;
+
+            // package as a successful response
+            var reqResult = new RequestResult<ResponseValue<T>>();
+            reqResult.WasHttpRequestSuccessful = true;
+            reqResult.WasRequestSuccessfullyHandled = true;
+            reqResult.HttpStatusCode = HttpStatusCode.OK;
+            reqResult.Result = result.Result;
+            return await Task.FromResult<RequestResult<ResponseValue<T>>>(reqResult);
+
+        }
+
+        public async Task<RequestResult<ResponseValue<ulong>>> GetBalanceAsync(string pubKey, Commitment commitment = Commitment.Finalized)
+        {
+            return await MockResult<ulong>();
+        }
+
+        public async Task<RequestResult<ResponseValue<BlockHash>>> GetRecentBlockHashAsync(Commitment commitment = Commitment.Finalized)
+        {
+            return await MockResult<BlockHash>();
+        }
+
+        public async Task<RequestResult<ResponseValue<List<TokenAccount>>>> GetTokenAccountsByOwnerAsync(string ownerPubKey, string tokenMintPubKey = null, string tokenProgramId = null, Commitment commitment = Commitment.Finalized)
+        {
+            return await MockResult<List<TokenAccount>>();
+        }
+
+        public async Task<RequestResult<string>> SendTransactionAsync(byte[] transaction, bool skipPreflight = false, Commitment commitment = Commitment.Finalized)
+        {
+            var mocked = await MockResult<string>();
+
+            var reqResult = new RequestResult<string>();
+            reqResult.HttpStatusCode = HttpStatusCode.OK;
+            reqResult.Result = mocked.Result.Value;
+
+            return await Task.FromResult<RequestResult<string>>(reqResult);
+
+        }
+    }
+}
--- test/Solnet.Extensions.Test/TokenWalletTest.cs
+++ test/Solnet.Extensions.Test/TokenWalletTest.cs
@@ -1,13 +1,236 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solnet.Rpc.Builders;
+using Solnet.Rpc.Messages;
+using Solnet.Wallet;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Solnet.Extensions.Test
 {
+
+    /// <summary>
+    /// Reusing testing mock base class from SolnetRpc.Test project
+    /// </summary>
     [TestClass]
-    public class TokenWalletTest
+    public class TokenWalletTest 
     {
+
+        private const string MnemonicWords =
+              "route clerk disease box emerge airport loud waste attitude film army tray" +
+              " forward deal onion eight catalog surface unit card window walnut wealth medal";
+
+        private const string Blockhash = "5cZja93sopRB9Bkhckj5WzCxCaVyriv2Uh5fFDPDFFfj";
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestMockJsonRpcParse()
         {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters =
+                {
+                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+                }
+            };
+            var json = File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json");
+            var result = JsonSerializer.Deserialize<JsonRpcResponse<ResponseValue<ulong>>>(json, serializerOptions);
+            Assert.IsNotNull(result);
         }
+
+
+        [TestMethod]
+        public void TestLoadKnownMint()
+        {
+            // get owner
+            var ownerWallet = new Wallet.Wallet(MnemonicWords);
+            var signer = ownerWallet.GetAccount(1);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", signer.PublicKey.Key);
+
+            // get mocked RPC client
+            var client = new MockTokenWalletRpc();
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+
+            // define some mints
+            var tokens = new TokenInfoResolver();
+            var testToken = new TokenInfo.TokenDef("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819", "TEST", "TEST", 2);
+            tokens.Add(testToken);
+
+            // load account
+            var wallet = TokenWallet.Load(client, tokens, "9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
+
+            // check wallet 
+            Assert.IsNotNull(wallet);
+
+            // check accounts
+            var accounts = wallet.TokenAccounts();
+            Assert.IsNotNull(accounts);
+
+            // locate known test mint account
+            var testAccounts = wallet.TokenAccounts().WithMint("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819");
+            Assert.AreEqual(1, testAccounts.Count());
+            Assert.AreEqual(0, testAccounts.WhichAreAssociatedTokenAccounts().Count());
+            Assert.AreEqual(2, wallet.TokenAccounts().WithSymbol("TEST").First().DecimalPlaces);
+            Assert.AreEqual(testToken.TokenMint, wallet.TokenAccounts().WithSymbol("TEST").First().TokenMint);
+            Assert.AreEqual(testToken.Symbol, wallet.TokenAccounts().WithMint("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819").First().Symbol);
+            Assert.AreEqual(10M, wallet.TokenAccounts().WithSymbol("TEST").First().BalanceDecimal);
+            Assert.AreEqual((ulong)125, wallet.TokenAccounts().WithSymbol("TEST").First().ConvertDecimalToUlong(1.25M));
+            Assert.AreEqual("G5SA5eMmbqSFnNZNB2fQV9ipHbh9y9KS65aZkAh9t8zv", wallet.TokenAccounts().WithSymbol("TEST").First().Address);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", wallet.TokenAccounts().WithSymbol("TEST").First().Owner);
+
+        }
+
+
+        [TestMethod]
+        public void TestLoadUnknownMint()
+        {
+            // get owner
+            var ownerWallet = new Wallet.Wallet(MnemonicWords);
+            var signer = ownerWallet.GetAccount(1);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", signer.PublicKey.Key);
+
+            // get mocked RPC client
+            var client = new MockTokenWalletRpc();
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+
+            // load account
+            var tokens = new TokenInfoResolver();
+            var wallet = TokenWallet.Load(client, tokens, "9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
+
+            // check wallet 
+            Assert.IsNotNull(wallet);
+
+            // check accounts
+            var accounts = wallet.TokenAccounts();
+            Assert.IsNotNull(accounts);
+
+            // locate unknown mint account
+            var unknownAccounts = wallet.TokenAccounts().WithMint("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex");
+            Assert.AreEqual(1, unknownAccounts.Count());
+            Assert.AreEqual(0, unknownAccounts.WhichAreAssociatedTokenAccounts().Count());
+            Assert.AreEqual(2, wallet.TokenAccounts().WithMint("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex").First().DecimalPlaces);
+            Assert.AreEqual(10M, wallet.TokenAccounts().WithMint("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex").First().BalanceDecimal);
+            Assert.AreEqual("4NSREK36nAr32vooa3L9z8tu6JWj5rY3k4KnsqTgynvm", wallet.TokenAccounts().WithMint("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex").First().Address);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", wallet.TokenAccounts().WithMint("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex").First().Owner);
+
+        }
+
+
+        [TestMethod]
+        public void TestProvisionAtaInjectBuilder()
+        {
+            // get owner
+            var ownerWallet = new Wallet.Wallet(MnemonicWords);
+            var signer = ownerWallet.GetAccount(1);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", signer.PublicKey.Key);
+
+            // get mocked RPC client
+            var client = new MockTokenWalletRpc();
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+
+            // define some mints
+            var tokens = new TokenInfoResolver();
+            var testToken = new TokenInfo.TokenDef("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819", "TEST", "TEST", 2);
+            tokens.Add(testToken);
+
+            // load account
+            var wallet = TokenWallet.Load(client, tokens, "9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
+
+            // check wallet 
+            Assert.IsNotNull(wallet);
+
+            // check accounts
+            var accounts = wallet.TokenAccounts();
+            Assert.IsNotNull(accounts);
+
+            // locate known test mint account
+            var testAccounts = wallet.TokenAccounts().WithMint("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819");
+            Assert.AreEqual(1, testAccounts.Count());
+            Assert.AreEqual(0, testAccounts.WhichAreAssociatedTokenAccounts().Count());
+          
+            // provision the ata
+            var builder = new TransactionBuilder();
+            builder
+                .SetFeePayer(signer)
+                .SetRecentBlockHash(Blockhash);
+            var before = builder.Build(signer);
+
+            // jit should append create ata command to builder
+            var testAta = wallet.JitCreateAssociatedTokenAccount(builder, testToken.TokenMint, new PublicKey("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5"));
+            var after = builder.Build(signer);
+            Assert.AreEqual("F6qCC87R5cmAJUKbhwERSFQHkQpSKyUkETgrjTJKB2nK", testAta.Key);
+            Assert.IsTrue(after.Length > before.Length);
+
+        }
+
+
+        [TestMethod]
+        public void TestLoadRefresh()
+        {
+            // get owner
+            var ownerWallet = new Wallet.Wallet(MnemonicWords);
+            var signer = ownerWallet.GetAccount(1);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", signer.PublicKey.Key);
+
+            // get mocked RPC client
+            var client = new MockTokenWalletRpc();
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+
+            // define some mints
+            var tokens = new TokenInfoResolver();
+            var testToken = new TokenInfo.TokenDef("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819", "TEST", "TEST", 2);
+            tokens.Add(testToken);
+
+            // load account
+            var wallet = TokenWallet.Load(client, tokens, "9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
+
+            // check wallet 
+            Assert.IsNotNull(wallet);
+
+            // refresh
+            wallet.Refresh();
+
+        }
+
+
+        [TestMethod]
+        public void TestProvisionAtaExecuteBuilder()
+        {
+            // get owner
+            var ownerWallet = new Wallet.Wallet(MnemonicWords);
+            var signer = ownerWallet.GetAccount(1);
+            Assert.AreEqual("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", signer.PublicKey.Key);
+
+            // get mocked RPC client
+            var client = new MockTokenWalletRpc();
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetBalanceResponse.json"));
+            client.Add(File.ReadAllText("Resources/TokenWallet/GetTokenAccountsByOwnerResponse.json"));
+
+            // define some mints
+            var tokens = new TokenInfoResolver();
+            var testToken = new TokenInfo.TokenDef("98mCaWvZYTmTHmimisaAQW4WGLphN1cWhcC7KtnZF819", "TEST", "TEST", 2);
+            tokens.Add(testToken);
+
+            // load account
+            var wallet = TokenWallet.Load(client, tokens, "9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
+
+            // check wallet 
+            Assert.IsNotNull(wallet);
+
+            // TODO - test send
+
+        }
+
     }
+
+
+
 }
