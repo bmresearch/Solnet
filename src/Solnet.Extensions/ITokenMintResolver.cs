a0a4860058a9af9d827fd63f93566843655c0333
liquizard <mliquizard@gmail.com>
2021-08-28 08:54:08 +0100

    Namespace reorg

--- src/Solnet.Extensions/ITokenMintResolver.cs
+++ src/Solnet.Extensions/ITokenMintResolver.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Solnet.Extensions.TokenMint;
+using Solnet.Rpc.Core.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Solnet.Extensions.TokenMint
+namespace Solnet.Extensions
 {
     /// <summary>
     /// Contains the method used to resolve mint public key addresses into TokenDef objects.
