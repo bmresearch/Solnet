76dd29fccc895081391818eb2b5ced73ca3657eb
Martin Whitman <martin.whitman.1776@gmail.com>
2021-10-05 22:07:27 -0600

    mw

---
+++ src/Solnet.Programs/StakeProgramData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solnet.Programs
+{
+    /// <summary>
+    /// Implements the stake program data encodings.
+    /// </summary>
+    internal static class StakeProgramData
+    {
+        /// <summary>
+        /// The offset at which the value which defines the program method begins. 
+        /// </summary>
+        internal const int MethodOffset = 0;
+        /// <summary>
+        /// Summary text here
+        /// </summary>
+        internal static byte[] EncodeInitializeData()
+        {
+
+        }
+    }
+}
