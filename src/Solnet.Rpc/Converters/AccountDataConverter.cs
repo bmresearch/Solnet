f5b97d86b55d1bb9eecc19cc3a08e3cb5f5fb7c1
Tiago Carvalho <tiagocarvalho92@hotmail.com>
2021-10-28 21:26:21 +0100

    Added QoL methods: GetTokenMintInfo and GetTokenAccountInfo. (#243)
    Added option to change Encoding during GetAccountInfo.
    Organized converters in their own namespace.

---
+++ src/Solnet.Rpc/Converters/AccountDataConverter.cs
@@ -0,0 +1,38 @@
+﻿using Solnet.Rpc.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Solnet.Rpc.Converters
+{
+    /// <inheritdoc/>
+    public class AccountDataConverter : JsonConverter<List<string>>
+    {
+        /// <inheritdoc/>
+        public override List<string> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.StartArray)
+                return JsonSerializer.Deserialize<List<string>>(ref reader, options);
+
+            if(reader.TokenType == JsonTokenType.StartObject)
+            {
+                JsonDocument doc = JsonDocument.ParseValue(ref reader);
+                var jsonAsString = doc.RootElement.ToString();
+
+                return new List<string>() { jsonAsString, "jsonParsed" };
+            }
+
+            throw new JsonException("Unable to parse account data");
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, List<string> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
