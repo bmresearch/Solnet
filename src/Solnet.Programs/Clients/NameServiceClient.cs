bff33de2426bb09ba2174e54fa565847da5dcd45
tiago <tmbcarvalho@outlook.com>
2021-12-13 13:23:27 +0000

    Initial changes

---
+++ src/Solnet.Programs/Clients/NameServiceClient.cs
@@ -0,0 +1,35 @@
+﻿using Solnet.Programs.Abstract;
+using Solnet.Programs.Models;
+using Solnet.Rpc;
+using Solnet.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solnet.Programs.Clients
+{
+    public class NameServiceClient : BaseClient
+    {
+        public static readonly PublicKey TokenTLD = new("6NSu2tci4apRKQtt257bAVcvqYjB3zV2H1dWo56vgpa6");
+
+        public NameServiceClient(IRpcClient rpcClient, IStreamingRpcClient streamingRpcClient) : base(rpcClient, streamingRpcClient)
+        {
+        }
+
+        public async Task<AccountResultWrapper<TokenData>> GetTokenFromMint(string key)
+        {
+            var hashedName = NameServiceProgram.ComputeHashedName(key);
+
+            var pda = NameServiceProgram.DeriveNameAccountKey(hashedName, null, TokenTLD);
+
+
+            return await GetAccount<TokenData>(pda);
+        }
+
+        public void GetMintFromToken(string token)
+        {
+        }
+    }
+}
---
+++ src/Solnet.Programs/Models/TokenData.cs
@@ -0,0 +1,49 @@
+﻿using Solnet.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solnet.Programs.Utilities;
+
+namespace Solnet.Programs.Models
+{
+    public class TokenData
+    {
+        public string Name { get; set; }
+
+        public string Ticker { get; set; }
+
+        public PublicKey Mint { get; set; }
+
+        public byte Decimals { get; set; }
+
+        public string Website { get; set; }
+
+        public string LogoUri { get; set; }
+
+        public static TokenData Deserialize(byte[] input)
+        {
+            var data = new ReadOnlySpan<byte>(input, 96, input.Length - 96);
+            int offset = 0;
+
+            offset += data.GetString(0, out var name);
+            offset += data.GetString(offset, out var ticker);
+
+            var mint = data.GetPubKey(offset);
+            offset += 32;
+
+            var decimals = data.GetU8(offset);
+            offset++;
+            string website = null, logo = null;
+
+            if (data.GetBool(offset++))
+                offset += data.GetString(offset, out website);
+
+            if (data.GetBool(offset++))
+                data.GetString(offset, out logo);
+
+            return new TokenData() { Name = name, Ticker = ticker, Decimals = decimals, LogoUri = logo, Mint = mint, Website = website };
+        }
+    }
+}
