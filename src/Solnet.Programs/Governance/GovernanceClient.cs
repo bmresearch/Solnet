7fdf630b6e5dbd878e841d91d26e4287b1881b0e
hoak <murlux@protonmail.com>
2021-12-14 11:08:35 +0000

    Work in progress governance program

---
+++ src/Solnet.Programs/Governance/GovernanceClient.cs
@@ -0,0 +1,34 @@
+﻿using Solnet.Programs.Abstract;
+using Solnet.Programs.Governance.Models;
+using Solnet.Rpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solnet.Programs.Governance
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GovernanceClient : BaseClient
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rpcClient"></param>
+        /// <param name="streamingRpcClient"></param>
+        public GovernanceClient(IRpcClient rpcClient, IStreamingRpcClient streamingRpcClient) : base(rpcClient, streamingRpcClient)
+        {
+
+        }
+
+
+        public async Task<List<Realm>> GetRealms()
+        {
+
+        }
+    }
+}
