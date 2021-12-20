031990f6f69b3df8956238368af882787e66d57e
hoak <murlux@protonmail.com>
2021-12-20 17:06:36 +0000

    Added GovernanceConfig

--- src/Solnet.Programs/Governance/Models/GovernanceConfig.cs
+++ src/Solnet.Programs/Governance/Models/GovernanceConfig.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Solnet.Programs.Governance.Models
 {
-    class GovernanceConfig
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GovernanceConfig
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public VoteThresholdPercentage VoteThresholdPercentage;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ulong MinCommunityTokensToCreateProposal;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint MinInstructionHoldUpTime;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint MaxVotingTime;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public VoteWeightSource VoteWeightSource;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint ProposalCoolOffTime;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ulong MinCouncilTokensToCreateProposal;
     }
 }
