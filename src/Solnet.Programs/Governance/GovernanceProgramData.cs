60c6f947ae1d739c3404f224c3e67e0831798fd3
hoak <murlux@protonmail.com>
2021-12-28 15:12:24 +0000

    Work in progress on governance instructions

--- src/Solnet.Programs/Governance/GovernanceProgramData.cs
+++ src/Solnet.Programs/Governance/GovernanceProgramData.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Solnet.Programs.Governance
 {
-    public class GovernanceProgramData
+    /// <summary>
+    /// Implements the governance program data encodings.
+    /// </summary>
+    public static class GovernanceProgramData
     {
+        /// <summary>
+        /// Encode the transaction instruction data for the <see cref="GovernanceProgramInstructions.Values.ExecuteInstruction"/> method.
+        /// </summary>
+        /// <returns>The byte array with the encoded data.</returns>
+        public static byte[] EncodeExecuteInstructionData()
+            => new[] { (byte)GovernanceProgramInstructions.Values.ExecuteInstruction };
     }
 }
