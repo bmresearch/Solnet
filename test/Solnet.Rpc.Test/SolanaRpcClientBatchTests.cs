f726c4fdd18510b2e389b88dcb814b3fd45bda7a
liquizard <mliquizard@gmail.com>
2022-01-28 10:40:07 +0000

    Remove batch async pattern, callbacks prevail

--- test/Solnet.Rpc.Test/SolanaRpcClientBatchTests.cs
+++ test/Solnet.Rpc.Test/SolanaRpcClientBatchTests.cs
@@ -109,58 +109,6 @@ namespace Solnet.Rpc.Test
         }
 
         [TestMethod]
-        public void TestCreateAndProcessBatchAsyncs()
-        {
-
-            var expected_requests = File.ReadAllText("Resources/Http/Batch/SampleBatchRequest.json");
-            var expected_responses = File.ReadAllText("Resources/Http/Batch/SampleBatchResponse.json");
-
-            ulong found_lamports = 0;
-            decimal found_balance = 0M;
-            int sig_callback_count = 0;
-
-            // compose a new batch of requests
-            var batch = new SolanaRpcBatchWithAsyncs();
-            var balance = batch.GetBalanceAsync("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
-            var tokensAccounts = batch.GetTokenAccountsByOwnerAsync("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", null, TokenProgram.ProgramIdKey);
-            var sigResults1 = batch.GetConfirmedSignaturesForAddress2Async("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", 200, null, null);
-            var sigResults2 = batch.GetConfirmedSignaturesForAddress2Async("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex", 200, null, null);
-            var sigResults3 = batch.GetConfirmedSignaturesForAddress2Async("4NSREK36nAr32vooa3L9z8tu6JWj5rY3k4KnsqTgynvm", 200, null, null);
-
-            // how many requests in batch?
-            Assert.AreEqual(5, batch.Composer.Count);
-
-            // serialize and check we're good
-            var reqs = batch.Composer.CreateJsonRequests();
-            var serializerOptions = CreateJsonOptions();
-            var json = JsonSerializer.Serialize<JsonRpcBatchRequest>(reqs, serializerOptions);
-            Assert.IsNotNull(reqs);
-            Assert.AreEqual(5, reqs.Count);
-            Assert.AreEqual(expected_requests, json);
-
-            // fake RPC response
-            var resp = CreateMockRequestResult<JsonRpcBatchResponse>(expected_requests, expected_responses, HttpStatusCode.OK);
-            Assert.IsNotNull(resp.Result);
-            Assert.AreEqual(5, resp.Result.Count);
-
-            // process and invoke callbacks
-            batch.Composer.ProcessBatchResponse(resp);
-
-            // pull task results that would otherwise haved blocked
-            found_lamports = balance.Result.Value;
-            found_balance = tokensAccounts.Result.Value[0].Account.Data.Parsed.Info.TokenAmount.AmountDecimal;
-            sig_callback_count += sigResults1.Result.Count;
-            sig_callback_count += sigResults2.Result.Count;
-            sig_callback_count += sigResults3.Result.Count;
-
-            // assertions
-            Assert.AreEqual((ulong)237543960, found_lamports);
-            Assert.AreEqual(12.5M, found_balance);
-            Assert.AreEqual(3, sig_callback_count);
-
-        }
-
-        [TestMethod]
         public void TestAutoExecuteMode()
         {
 
@@ -179,24 +127,24 @@ namespace Solnet.Rpc.Test
             var mockRpcClient = ClientFactory.GetClient(Cluster.MainNet, null, mockHttpClient);
 
             // compose a new batch of requests
-            var batch = new SolanaRpcBatchWithAsyncs();
+            var batch = new SolanaRpcBatchWithCallbacks();
             batch.AutoExecute(BatchAutoExecuteMode.ExecuteWithFatalFailure, mockRpcClient, 10);
-            var balance = batch.GetBalanceAsync("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5");
-            var tokensAccounts = batch.GetTokenAccountsByOwnerAsync("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", null, TokenProgram.ProgramIdKey);
-            var sigResults1 = batch.GetConfirmedSignaturesForAddress2Async("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", 200, null, null);
-            var sigResults2 = batch.GetConfirmedSignaturesForAddress2Async("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex", 200, null, null);
-            var sigResults3 = batch.GetConfirmedSignaturesForAddress2Async("4NSREK36nAr32vooa3L9z8tu6JWj5rY3k4KnsqTgynvm", 200, null, null);
+            batch.GetBalance("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5",
+                callback: (x, ex) => found_lamports = x.Value);
+            batch.GetTokenAccountsByOwner("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", null, TokenProgram.ProgramIdKey,
+                callback: (x, ex) => found_balance = x.Value[0].Account.Data.Parsed.Info.TokenAmount.AmountDecimal);
+            batch.GetConfirmedSignaturesForAddress2("9we6kjtbcZ2vy3GSLLsZTEhbAqXPTRvEyoxa8wxSqKp5", 200, null, null,
+                callback: (x, ex) => sig_callback_count += x.Count);
+            batch.GetConfirmedSignaturesForAddress2("88ocFjrLgHEMQRMwozC7NnDBQUsq2UoQaqREFZoDEex", 200, null, null,
+                callback: (x, ex) => sig_callback_count += x.Count);
+            batch.GetConfirmedSignaturesForAddress2("4NSREK36nAr32vooa3L9z8tu6JWj5rY3k4KnsqTgynvm", 200, null, null,
+                callback: (x, ex) => sig_callback_count += x.Count);
+            
+            // run through any remaining requests in batch
             batch.Flush();
 
             // how many requests in batch? should be zero - already flushed/executed
             Assert.AreEqual(0, batch.Composer.Count);
-
-            // pull task results that would otherwise haved blocked
-            found_lamports = balance.Result.Value;
-            found_balance = tokensAccounts.Result.Value[0].Account.Data.Parsed.Info.TokenAmount.AmountDecimal;
-            sig_callback_count += sigResults1.Result.Count;
-            sig_callback_count += sigResults2.Result.Count;
-            sig_callback_count += sigResults3.Result.Count;
 
             // assertions
             Assert.AreEqual((ulong)237543960, found_lamports);
