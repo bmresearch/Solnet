ce31e5ab4c35e9521b42e743a52df1d5dff3863b
Tiago Carvalho <tmbcarvalho@outlook.com>
2022-02-22 22:18:16 +0000

    Squashing some warnings.

--- src/Solnet.Programs/Abstract/Program.cs
+++ src/Solnet.Programs/Abstract/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Solnet.Programs.Abstract
 {
+    /// <summary>
+    /// Base Program interface.
+    /// </summary>
     public interface Program
     {
         /// <summary>
