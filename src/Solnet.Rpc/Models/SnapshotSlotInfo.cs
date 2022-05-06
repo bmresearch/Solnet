e973e1a866c223a8b7f059509e28801b628f1118
hoak <murlux@protonmail.com>
2022-05-06 13:55:28 +0100

    Adds the getFeeForMessage and getHighestSnapshotSlot v1.9 methods (#379)

---
+++ src/Solnet.Rpc/Models/SnapshotSlotInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Solnet.Rpc.Models
+{
+    /// <summary>
+    /// The highest snapshot slot info.
+    /// </summary>
+    public class SnapshotSlotInfo
+    {
+        /// <summary>
+        /// The highest full snapshot slot.
+        /// </summary>
+        public ulong Full { get; set; }
+
+        /// <summary>
+        /// The highest incremental snapshot slot based on <see cref="Full"/>.
+        /// </summary>
+        public ulong? Incremental { get; set; }
+    }
+}
--- test/Solnet.Rpc.Test/SolanaRpcClientFeeTests.cs
+++ test/Solnet.Rpc.Test/SolanaRpcClientFeeTests.cs
@@ -73,6 +73,31 @@ namespace Solnet.Rpc.Test
 
             FinishTest(messageHandlerMock, TestnetUri);
         }
+        
+        [TestMethod]
+        public void TestGetFeeForMessage()
+        {
+            var responseData = File.ReadAllText("Resources/Http/Fees/GetFeeForMessageResponse.json");
+            var requestData = File.ReadAllText("Resources/Http/Fees/GetFeeForMessageRequest.json");
+            var sentMessage = string.Empty;
+            var messageHandlerMock = SetupTest(
+                (s => sentMessage = s), responseData);
+
+            var httpClient = new HttpClient(messageHandlerMock.Object)
+            {
+                BaseAddress = TestnetUri,
+            };
+
+            var sut = new SolanaRpcClient(TestnetUrl, null, httpClient);
+            var result = sut.GetFeeForMessage("AQABAu\u002BOVfa66vZfLI0xdX9GcGk/\u002BU65\u002Bdox\u002BiHABM3DOSGuBUpTWpkpIQZNJOhxYNo4fHw1td28kruB5B\u002BoQEEFRI3tj0g2caCBX14VjqrxK4Daz/4WvmWxU698Okvp8lYDjAEBACNIZWxsbyBTb2xhbmEgV29ybGQsIHVzaW5nIFNvbG5ldCA6KQ==");
+
+            Assert.AreEqual(requestData, sentMessage);
+            Assert.IsNotNull(result.Result);
+            Assert.IsTrue(result.WasSuccessful);
+            Assert.AreEqual(132177311UL, result.Result.Context.Slot);
+            Assert.AreEqual(5000UL, result.Result.Value);
+            FinishTest(messageHandlerMock, TestnetUri);
+        }
 
         [TestMethod]
         public void TestGetFeesConfirmed()
