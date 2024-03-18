b8985d607b9c9b338068b3c09bab7c3fc7f552e5
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-03-18 17:29:14 -0300

    Implement Compute Budget Program instructions, encoders and decoders

---
+++ src/Solnet.Programs/ComputeBudget/ComputeBudgetProgram.cs
@@ -0,0 +1,140 @@
+using Solnet.Programs.Utilities;
+using Solnet.Rpc.Models;
+using Solnet.Wallet;
+using System;
+using System.Collections.Generic;
+
+namespace Solnet.Programs.ComputeBudget
+{
+    /// <summary>
+    /// Implements the Compute Budget Program methods.
+    /// <remarks>
+    /// For more information see:
+    /// https://docs.rs/solana-sdk/1.18.7/solana_sdk/compute_budget/enum.ComputeBudgetInstruction.html
+    /// </remarks>
+    /// </summary>
+    public static class ComputeBudgetProgram
+    {
+        /// <summary>
+        /// The public key of the Compute Budget Program.
+        /// </summary>
+        public static readonly PublicKey ProgramIdKey = new("ComputeBudget111111111111111111111111111111");
+
+        /// <summary>
+        /// The program's name.
+        /// </summary>
+        private const string ProgramName = "Compute Budget Program";
+
+        /// <summary>
+        /// Request a specific transaction-wide program heap region size in bytes. The value requested must be a multiple of 1024. This new heap region size applies to each program executed in the transaction, including all calls to CPIs.
+        /// </summary>
+        /// <param name="bytes">The heap region size.</param>
+        /// <returns>The transaction instruction.</returns>
+        public static TransactionInstruction RequestHeapFrame(uint bytes)
+        {
+            return new TransactionInstruction
+            {
+                ProgramId = ProgramIdKey.KeyBytes,
+                Keys = new List<AccountMeta>(),
+                Data = ComputeBudgetProgramData.EncodeRequestHeapFrameData(bytes)
+            };
+        }
+
+        /// <summary>
+        /// Set a specific compute unit limit that the transaction is allowed to consume.
+        /// </summary>
+        /// <param name="units">The compute unit limit.</param>
+        /// <returns>The transaction instruction.</returns>
+        public static TransactionInstruction SetComputeUnitLimit(uint units)
+        {
+            return new TransactionInstruction()
+            {
+                ProgramId = ProgramIdKey.KeyBytes,
+                Keys = new List<AccountMeta>(),
+                Data = ComputeBudgetProgramData.EncodeSetComputeUnitLimitData(units)
+            };
+        }
+
+        /// <summary>
+        /// Set a compute unit price in “micro-lamports” to pay a higher transaction fee for higher transaction prioritization.
+        /// </summary>
+        /// <param name="microLamports">The compute unit price.</param>
+        /// <returns>The transaction instruction.</returns>
+        public static TransactionInstruction SetComputeUnitPrice(ulong microLamports)
+        {
+            return new TransactionInstruction()
+            {
+                ProgramId = ProgramIdKey.KeyBytes,
+                Keys = new List<AccountMeta>(),
+                Data = ComputeBudgetProgramData.EncodeSetComputeUnitPriceData(microLamports)
+            };
+        }
+        
+        /// <summary>
+        /// Set a specific transaction-wide account data size limit, in bytes, is allowed to load.
+        /// </summary>
+        /// <param name="bytes">The account data size limit.</param>
+        /// <returns>The transaction instruction.</returns>
+        public static TransactionInstruction SetLoadedAccountsDataSizeLimit(uint bytes)
+        {
+            return new TransactionInstruction()
+            {
+                ProgramId = ProgramIdKey.KeyBytes,
+                Keys = new List<AccountMeta>(),
+                Data = ComputeBudgetProgramData.EncodeSetLoadedAccountsDataSizeLimit(bytes)
+            };
+        }
+
+        /// <summary>
+        /// Decodes an instruction created by the Compute Budget Program.
+        /// </summary>
+        /// <param name="data">The instruction data to decode.</param>
+        /// <returns>A decoded instruction.</returns>
+        public static DecodedInstruction Decode(ReadOnlySpan<byte> data)
+        {
+            uint instruction = data.GetU32(ComputeBudgetProgramData.MethodOffset);
+
+            if (!Enum.IsDefined(typeof(ComputeBudgetProgramInstructions.Values), instruction))
+            {
+                return new()
+                {
+                    PublicKey = ProgramIdKey,
+                    InstructionName = "Unknown Instruction",
+                    ProgramName = ProgramName,
+                    Values = new Dictionary<string, object>(),
+                    InnerInstructions = new List<DecodedInstruction>()
+                };
+            }
+
+            ComputeBudgetProgramInstructions.Values instructionValue = (ComputeBudgetProgramInstructions.Values)instruction;
+
+
+            DecodedInstruction decodedInstruction = new()
+            {
+                PublicKey = ProgramIdKey,
+                InstructionName = ComputeBudgetProgramInstructions.Names[instructionValue],
+                ProgramName = ProgramName,
+                Values = new Dictionary<string, object>(),
+                InnerInstructions = new List<DecodedInstruction>()
+            };
+
+            switch (instructionValue)
+            {
+                case ComputeBudgetProgramInstructions.Values.RequestHeapFrame:
+                    ComputeBudgetProgramData.DecodeRequestHeapFrameData(decodedInstruction, data);
+                    break;
+                case ComputeBudgetProgramInstructions.Values.SetComputeUnitLimit:
+                    ComputeBudgetProgramData.DecodeSetComputeUnitLimitData(decodedInstruction, data);
+                    break;
+                case ComputeBudgetProgramInstructions.Values.SetComputeUnitPrice:
+                    ComputeBudgetProgramData.DecodeSetComputeUnitPriceData(decodedInstruction, data);
+                    break;
+                case ComputeBudgetProgramInstructions.Values.SetLoadedAccountsDataSizeLimit:
+                    ComputeBudgetProgramData.DecodeSetLoadedAccountsDataSizeLimitData(decodedInstruction, data);
+                    break;
+            }
+
+            return decodedInstruction;
+        }
+    }
+}
---
+++ src/Solnet.Programs/ComputeBudget/ComputeBudgetProgramData.cs
@@ -0,0 +1,116 @@
+using Solnet.Programs.Utilities;
+using System;
+
+namespace Solnet.Programs.ComputeBudget
+{
+    /// <summary>
+    /// Implements the compute budget program data encodings.
+    /// </summary>
+    internal static class ComputeBudgetProgramData
+    {
+        /// <summary>
+        /// The offset at which the value which defines the program method begins. 
+        /// </summary>
+        internal const int MethodOffset = 0;
+
+        /// <summary>
+        /// Encode transaction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.RequestHeapFrame"/> method.
+        /// </summary>
+        /// <param name="bytes">The heap region size.</param>
+        /// <returns>The transaction instruction data.</returns>
+        internal static byte[] EncodeRequestHeapFrameData(uint bytes)
+        {
+            byte[] data = new byte[5];
+            
+            data.WriteU8((byte)ComputeBudgetProgramInstructions.Values.SetComputeUnitLimit, MethodOffset);
+            data.WriteU32(bytes, 1);
+            
+            return data;
+        }
+
+        /// <summary>
+        /// Encode transaction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetComputeUnitLimit"/> method.
+        /// </summary>
+        /// <param name="units">The compute unit limit.</param>
+        /// <returns>The transaction instruction data.</returns>
+        internal static byte[] EncodeSetComputeUnitLimitData(uint units)
+        {
+            byte[] data = new byte[5];
+            
+            data.WriteU8((byte)ComputeBudgetProgramInstructions.Values.SetComputeUnitLimit, MethodOffset);
+            data.WriteU32(units, 1);
+            
+            return data;
+        }
+
+        /// <summary>
+        /// Encode transaction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetComputeUnitPrice"/> method.
+        /// </summary>
+        /// <param name="microLamports">The compute unit price.</param>
+        /// <returns>The transaction instruction data.</returns>
+        internal static byte[] EncodeSetComputeUnitPriceData(ulong microLamports)
+        {
+            byte[] data = new byte[9];
+
+            data.WriteU8((byte)ComputeBudgetProgramInstructions.Values.SetComputeUnitPrice, MethodOffset);
+            data.WriteU64(microLamports, 1);
+
+            return data;
+        }
+        
+        /// <summary>
+        /// Encode transaction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetLoadedAccountsDataSizeLimit"/> method.
+        /// </summary>
+        /// <param name="bytes">The account data size limit.</param>
+        /// <returns>The transaction instruction data.</returns>
+        internal static byte[] EncodeSetLoadedAccountsDataSizeLimit(uint bytes)
+        {
+            byte[] data = new byte[5];
+
+            data.WriteU8((byte)ComputeBudgetProgramInstructions.Values.SetLoadedAccountsDataSizeLimit, MethodOffset);
+            data.WriteU32(bytes, 1);
+
+            return data;
+        }
+
+        /// <summary>
+        /// Decodes the instruction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.RequestHeapFrame"/> method
+        /// </summary>
+        /// <param name="decodedInstruction">The decoded instruction to add data to.</param>
+        /// <param name="data">The instruction data to decode.</param>
+        internal static void DecodeRequestHeapFrameData(DecodedInstruction decodedInstruction, ReadOnlySpan<byte> data)
+        {
+            decodedInstruction.Values.Add("Bytes", data.GetU32(1));
+        }
+        
+        /// <summary>
+        /// Decodes the instruction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetComputeUnitLimit"/> method
+        /// </summary>
+        /// <param name="decodedInstruction">The decoded instruction to add data to.</param>
+        /// <param name="data">The instruction data to decode.</param>
+        internal static void DecodeSetComputeUnitLimitData(DecodedInstruction decodedInstruction, ReadOnlySpan<byte> data)
+        {
+            decodedInstruction.Values.Add("Units", data.GetU32(1));
+        }
+        
+        /// <summary>
+        /// Decodes the instruction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetComputeUnitPrice"/> method
+        /// </summary>
+        /// <param name="decodedInstruction">The decoded instruction to add data to.</param>
+        /// <param name="data">The instruction data to decode.</param>
+        internal static void DecodeSetComputeUnitPriceData(DecodedInstruction decodedInstruction, ReadOnlySpan<byte> data)
+        {
+            decodedInstruction.Values.Add("Micro Lamports", data.GetU64(1));
+        }
+        
+        /// <summary>
+        /// Decodes the instruction instruction data for the <see cref="ComputeBudgetProgramInstructions.Values.SetLoadedAccountsDataSizeLimit"/> method
+        /// </summary>
+        /// <param name="decodedInstruction">The decoded instruction to add data to.</param>
+        /// <param name="data">The instruction data to decode.</param>
+        internal static void DecodeSetLoadedAccountsDataSizeLimitData(DecodedInstruction decodedInstruction, ReadOnlySpan<byte> data)
+        {
+            decodedInstruction.Values.Add("Bytes", data.GetU32(1));
+        }
+    }
+}
