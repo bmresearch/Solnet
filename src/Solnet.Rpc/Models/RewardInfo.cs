603a7e5083987ab20bd4c83d828783e769ffb30a
Nathan Martell <projectbifrost@gmail.com>
2025-02-12 17:36:04 -0500

    Updated getTransaction to allow different response encodings & added support for loadedAddresses, rewards and dynamic version json types
    Updated getTransaction to allow different response encodings & added support for loadedAddresses, rewards and dynamic version json types.
    
    Organized transaction models and gave them their own space.
    
    Addresses loaded from the AddressLookupTable will be found in loadedAddresses when retrieving versioned transactions

---
+++ src/Solnet.Rpc/Models/RewardInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace Solnet.Rpc.Models
+{
+    /// <summary>
+    /// Represents the reward information related to a given account.
+    /// </summary>
+    public class RewardInfo
+    {
+        /// <summary>
+        /// The account pubkey as base58 encoded string.
+        /// </summary>
+        public string Pubkey { get; set; }
+        /// <summary>
+        /// Number of reward lamports credited or debited by the account.
+        /// </summary>
+        public long Lamports { get; set; }
+
+        /// <summary>
+        /// Account balance in lamports after the reward was applied.
+        /// </summary>
+        public ulong PostBalance { get; set; }
+
+    }
+  
+    /// <summary>
+    /// The type of the reward.
+    /// </summary>
+    public enum RewardType
+    {
+        /// <summary>
+        /// Default value in case the returned value is undefined.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Fee reward.
+        /// </summary>
+        Fee,
+
+        /// <summary>
+        /// Rent reward.
+        /// </summary>
+        Rent,
+
+        /// <summary>
+        /// Voting reward.
+        /// </summary>
+        Voting,
+
+        /// <summary>
+        /// Staking reward.
+        /// </summary>
+        Staking
+    }
+}
